1feb7f4c24a7d440405ebe566a3c50504722905c
Nerim <nerim5037@gmail.com>
2025-04-28 20:05:57 +0900

    chore: GlobalStyle import 경로 수정

--- src/context/GlobalStyleProvider.tsx
+++ src/context/GlobalStyleProvider.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ReactNode } from "react";
-import GlobalStyle from "@/styles/globalStyle";
+import GlobalStyle from "../styles/GlobalStyle";
 
 export default function GlobalStyleProvider({
   children,
