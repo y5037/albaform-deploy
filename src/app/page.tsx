aeaf60a283ca3a0f538b674df862898782f8b3b8
Nerim <nerim5037@gmail.com>
2025-04-29 11:10:29 +0900

    chore: tailwind 지시어 CSS 파일명 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className='flex justify-center items-center min-h-screen'>
-      <div className='text-3xl font-semibold'>랜딩</div>
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="text-3xl font-semibold">랜딩</div>
     </div>
   );
 }
