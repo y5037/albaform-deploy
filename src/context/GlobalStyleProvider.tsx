053cfe13e0d8a935a0767d5c395be3902164495a
Nerim <nerim5037@gmail.com>
2025-05-03 17:39:38 +0900

    fix: media.ts 파일 이동 및 글로벌 style 파일명 소문자 변경

--- src/context/GlobalStyleProvider.tsx
+++ src/context/GlobalStyleProvider.tsx
@@ -2,7 +2,7 @@
 
 import { ReactNode } from "react";
 
-import GlobalStyle from "../styles/GlobalStyle";
+import GlobalStyle from "../styles/globalStyle";
 
 export default function GlobalStyleProvider({
   children,
