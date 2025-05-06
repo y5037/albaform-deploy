d630b47248649e879bf0e4ce29cf377b9a3cf335
katej0320 <bobaejang1@gmail.com>
2025-05-06 12:33:37 +0900

    feat: 내브바 구현 완료/ 내브 모달창 구현완료 (#124)
    * feat:navbar 페이지에 맞게 variant로 구분하여 구현
    
    * chore: 스토리북 다운로드
    
    * chore: 테일윈드 다운로드 및 빌드오류 해결
    
    * fix: 랜딩 내브바 세분화 없엠
    
    * fix: css 색상 변수명 글로벌 변수명 변경
    
    * feat: ClientLayout 적용하여 내브바 페이지별로 다르게 구현
    
    * fix: 색상변수 적용 및 ClientLayout 불필요한 코드 수정
    
    * chore: navbar 컴포넌트 폴더로 이동
    
    * fix: 색상변수 Orange-> orange 소문자로 수정
    
    * feat: 내브바 모달 구현중
    
    * feat: 내브바 모달 css 구현중
    
    * fix: 내비바 페이지에 안보이는 버그수정
    
    * feat: 내브 모달 구현 완료
    
    * feat: 로그인/회원가입 페이지 에서 메뉴아이템 오른쪽 정렬
    
    * fix: useRouter 버그 수정
    
    * chore: 버그수정
    
    * feat: 랜딩페이지내브바에 로그인 버튼추가
    
    * fix: 내브바 포지션 fixed 및 넓이 세팅 1920px;
    
    * chore: albaformList page.ts 타입검사 자동생성된 파일 삭제
    
    * fix: albaformList page.ts 파일 삭제
    
    * fix: 내브바 미디어 쿼리 적용

---
+++ src/components/navbar/NavModal.tsx
@@ -0,0 +1,95 @@
+'use client';
+
+import { MouseEvent } from "react"
+import styled from "styled-components";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+
+const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0; 
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: flex-end; 
+  z-index: 1000;
+`;
+
+const ModalContainer = styled.div`
+  background: white;
+  width: 560px; 
+  height: 100vh; 
+  text-align: left;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+`;
+
+const CloseButtonWrapper = styled.div`
+  width: 560px;
+  height: 88px;
+  border-bottom: 1px solid var(--gray100);
+  position: relative;
+`
+const CloseButton = styled.img`
+  width: 36px;
+  height: 36px;
+  cursor: pointer;
+  top: 26px;
+  right: 56px;
+  position: absolute;
+`;
+
+
+const ModalContent = styled.div`
+  font-size: 20px;
+  width: 100%;
+  height: 100px;
+  padding: 32px 56px;
+  display: flex;
+  align-items: center;
+  gap: 24px;
+  cursor: pointer;
+  
+
+`
+const myPageIcon = '/images/mypage.png';
+const logoutIcon = '/images/logout.png';
+const closeButtonImg = '/images/X.png';
+
+
+export default function NavModal({ onClose }: {onClose: ()=>void}){
+    const router = useRouter();
+    const handleBackgroundClick = (e: MouseEvent<HTMLDivElement>) => {
+        if(e.target === e.currentTarget) {
+            onClose();
+        }
+    }
+
+    return(
+    <div>
+      <Overlay onClick={handleBackgroundClick}>
+        <ModalContainer>
+          <CloseButtonWrapper>
+            <CloseButton src={closeButtonImg} onClick={onClose}/>
+          </CloseButtonWrapper>
+          <ModalContent>
+            <Image src={myPageIcon} alt="mypage" width={36} height={36} onClick={() => router.push('/mypage')}/>
+            <p onClick={() => router.push('/mypage')}>마이 페이지</p>
+          </ModalContent>
+          <ModalContent>
+            <Image src={logoutIcon} alt="logout" width={36} height={36} onClick={() => router.push('/logout')} />
+            <p onClick={() => router.push('/logout')}>로그아웃</p>
+          </ModalContent>
+        </ModalContainer>
+      </Overlay>
+
+        
+        
+    </div>
+
+    )
+}
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,95 +1,144 @@
-"use client"
+'use client';
 
-import styled from "styled-components";
-import { useState } from "react";
+import styled from 'styled-components';
+import { useState } from 'react';
+import NavModal from './NavModal';
+import { usePathname, useRouter } from 'next/navigation';
+import { media } from '@/styles/media';
+
+type NavVariant = 'default' | 'login';
+
+interface Props {
+  variant?: NavVariant;
+}
+
+export default function Navbar({ variant = 'default' }: Props) {
+  const [activeMenu, setActiveMenu] = useState<string>('');
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const pathname = usePathname();
+  const router = useRouter();
+  const isLandingPage = pathname === '/';
+
+  const bgColor = {
+    default: 'var(--white)',
+    login: 'var(--white)',
+  };
+
+  const menuItems = {
+    default: ['알바 목록', '알바 토크', '내 알바폼'],
+    login: ['사장님 전용', '지원자 전용'],
+  };
+
+  const hamburgerMenu = {
+    default: '/images/menu.png',
+    login: 'none',
+  };
+
+  const Logo = styled.img`
+    height: 40px;
+    cursor: pointer;
+    margin-right: 48px;
+  `;
+
+  const NavbarWrapper = styled.nav<{ $bg: string }>`
+    position: fixed;
+    width: 1920px;
+    z-index: 999;
+    display: flex;
+    align-items: center;
+    padding: 24px 220px;
+    background-color: ${(props) => props.$bg};
+    border-bottom: 1px solid var(--gray100);
+
+    /* Desktop: 중앙 정렬 + max-width 제한 */
+  @media ${media.desktop} {
+    padding: 0;
+    max-width: 1480px;
+    margin: 0 auto;
+  }
+
+  /* Tablet 이하: 패딩 24px, 좌우 꽉 채움 */
+  @media ${media.tablet} {
+    padding: 0 24px;
+    margin: 0;
+  }
 
 
+  `;
+  const MenuList = styled.ul<{ $alignRight?: boolean }>`
+    display: flex;
+    gap: 24px;
+    flex: 1;
+    justify-content: ${(props) =>
+      props.$alignRight ? 'flex-end' : 'flex-start'};
+  `;
 
-    type NavVariant = 'default' |  'login';
-
-    interface Props {
-        variant?:NavVariant;
+  const MenuItem = styled.li<{ $isActive?: boolean }>`
+    font-size: 20px;
+    color: ${(props) =>
+      props.$isActive ? 'var( --primary-orange300)' : 'var( --gray300)'};
+    cursor: pointer;
+    &:hover {
+      color: var(--primary-orange300);
     }
+  `;
 
-    export default function Navbar({ variant = 'default'}:Props){
-        const [activeMenu, setActiveMenu ] = useState<string>('');
+  const Hamburger = styled.img`
+    width: 36px;
+    height: 36px;
+    cursor: pointer;
+  `;
 
-        const bgColor = {
-            default : 'var(--white)',
-            login : 'var(--white)',
-
-        }
-
-    
-
-        const menuItems = {
-            default : ['알바 목록', '알바 토크', '내 알바폼'],
-            login : ['사장님 전용', '지원자 전용']
-        }
-
-        const hamburgerMenu = {
-            default : '/images/menu.png',
-            login : 'none',
-        }
-
-        const Logo = styled.img `
-            height: 40px;
-            cursor : pointer;
-            margin-right: 48px;
-        `;
-
-        const NavbarWrapper = styled.nav<{$bg: string}>`
-            display: flex;
-            align-items: center;
-            padding: 24px 220px;
-            background-color: ${(props)=>props.$bg};      
-        `;
-
-        const MenuList = styled.ul`
-            display: flex;
-            gap: 24px;
-        
-        `;
-
-        const MenuItem = styled.li<{ $isActive?: boolean }>`
-            font-size: 20px;
-            color: ${(props) => (props.$isActive ? 'var( --primary-orange300)' : 'var( --gray300)')};
-            cursor: pointer;
-            &:hover{
-                color: var(--primary-orange300);
-                }
-        `;
-
-        const Hamburger = styled.img`
-            width: 36px;
-            height: 36px;
-            cursor: pointer;
-        
-        `
-
-
-
-    return(
-        <div>
-            <NavbarWrapper $bg={bgColor[variant]}>
-                <Logo src='/logo/logo.png' alt="logo" />
-                    <MenuList>
-                        {menuItems[variant].map((item)=>(
-                            <MenuItem 
-                                key={item}
-                                $isActive={item === activeMenu}
-                                onClick={()=>setActiveMenu(item)}
-                                >
-                                    item
-                                </MenuItem>
-                        ))}
-                    </MenuList>
-                        {hamburgerMenu[variant] !== 'none' && (
-                            <Hamburger src={hamburgerMenu[variant]} alt="menu icon"/>
-                        )}
-                
-            </NavbarWrapper>
-
-        </div>
-    );
+  return (
+    <div>
+      <NavbarWrapper $bg={bgColor[variant]}>
+        <Logo src='/logo/logo.png' alt='logo' />
+        <MenuList $alignRight={variant === 'login'}>
+          {menuItems[variant].map((item) => (
+            <MenuItem
+              key={item}
+              $isActive={item === activeMenu}
+              onClick={() => setActiveMenu(item)}
+            >
+              {item}
+            </MenuItem>
+          ))}
+        </MenuList>
+        {isLandingPage ? (
+  <button
+    onClick={() => router.push('/signin')}
+    style={{
+      fontSize: '16px',
+      background: 'var(--primary-orange300)',
+      border: '1px solid var(--primary-orange300)',
+      padding: '8px 16px',
+      borderRadius: '6px',
+      cursor: 'pointer',
+      color: 'white',
+    }}
+  >
+    로그인
+  </button>
+) : (
+  hamburgerMenu[variant] !== 'none' && (
+    <Hamburger
+      src={hamburgerMenu[variant]}
+      alt='menu icon'
+      onClick={handleOpenModal}
+    />
+  )
+)}
+        {isModalOpen && <NavModal onClose={handleCloseModal} />}
+      </NavbarWrapper>
+    </div>
+  );
 }
