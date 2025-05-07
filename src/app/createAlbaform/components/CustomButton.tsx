98dd1f46df3f989cf4f21e25d0221a5e9876f88d
이승환 <ls991217@naver.com>
2025-05-07 17:55:23 +0900

    feat: 커스텀버튼 작성

---
+++ src/app/createAlbaform/components/CustomButton.tsx
@@ -0,0 +1,105 @@
+'use client';
+
+import React from 'react';
+import styled, { css } from 'styled-components';
+
+type ButtonSize = 'small' | 'large';
+type ButtonVariant = 'primary' | 'large_primary' | 'secondary';
+
+interface CustomButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  size?: ButtonSize;
+  variant?: ButtonVariant;
+}
+
+const size = {
+  // 취소 버튼
+  small: css`
+    font-size: 20px;
+    line-height: 32px;
+    padding: 12px 24px;
+
+    @media (max-width: 1024px) {
+      font-size: 14px;
+      line-height: 24px;
+      padding: 8px 14px;
+    }
+  `,
+  // 저장, 등록 버튼
+  large: css`
+    font-size: 20px;
+    line-height: 32px;
+    padding: 12px 24px;
+
+    @media (max-width: 1024px) {
+      padding: 16px;
+      font-size: 16px;
+      line-height: 26px;
+    }
+  `,
+};
+
+const variant = {
+  // 취소 버튼
+  primary: css`
+    background-color: var(--gray100);
+    color: var(--white);
+    width: 74px;
+    border: none;
+
+    &:hover {
+      background-color: var(--primary-orange400);
+    }
+
+    @media (max-width: 1024px) {
+      width: 52px;
+    }
+  `,
+  // 등록 버튼
+  large_primary: css`
+    background-color: var(--gray100);
+    color: var(--white);
+    width: 100%;
+    border: none;
+
+    &:hover {
+      background-color: var(--primary-orange400);
+    }
+  `,
+  // 저장 버튼
+  secondary: css`
+    background-color: var(--white);
+    color: var(--gray400);
+    width: 100%;
+    border: 1px solid var(--gray100);
+
+    &:hover {
+      color: var(--primary-orange400);
+      border: 1px solid var(--primary-orange400);
+    }
+  `,
+};
+
+const StyledButton = styled.button<CustomButtonProps>`
+  text-align: center;
+  font-weight: 600;
+  border-radius: 8px;
+  transition: all 0.2s;
+  cursor: pointer;
+
+  ${(props) => size[props.size || 'small']}
+  ${(props) => variant[props.variant || 'primary']}
+`;
+
+export default function CustomButton({
+  children,
+  size = 'small',
+  variant = 'primary',
+  ...rest
+}: CustomButtonProps) {
+  return (
+    <StyledButton size={size} variant={variant} {...rest}>
+      {children}
+    </StyledButton>
+  );
+}
--- src/app/createAlbaform/page.tsx
+++ src/app/createAlbaform/page.tsx
@@ -1,15 +1,18 @@
+import CustomButton from './components/CustomButton';
+
 export default function CreateForm() {
   return (
     <>
-      <div className='flex justify-between items-center max-w-[327px] pt-[112px] mx-auto md:max-w-[640px]'>
-        <h1 className='font-semibold text-xl leading-[32px]'>알바폼 만들기</h1>
-        <button
+      <div className='flex justify-between items-center max-w-[327px] pt-[112px] mx-auto lg:max-w-[640px]'>
+        <h1
           className='
-          bg-[var(--gray100)] text-[var(--white)] font-semibold text-center text-sm
-          rounded-lg px-[14px] py-[8px]'
+          font-semibold text-[20px] leading-[32px]
+          lg:text-[32px] lg:leading-[46px]
+        '
         >
-          작성취소
-        </button>
+          알바폼 만들기
+        </h1>
+        <CustomButton>작성취소</CustomButton>
       </div>
     </>
   );
