127618072477118b360f1179e1b672d32c8d372f
y5037 <71241364+y5037@users.noreply.github.com>
2025-05-10 18:46:36 +0900

    chore: zod error message 수정

--- src/schemas/editProfileSchema.ts
+++ src/schemas/editProfileSchema.ts
@@ -1,10 +1,10 @@
 import { z } from 'zod';
 
 export const editProfileSchema = z.object({
-  nickname: z.string().min(1, { message: '닉네임은 필수입니다' }),
-  store: z.string().min(1, {message:'가게 이름(상호명)을 필수로 입력해주세요'}),
-  storeTel: z.string().min(1, {message:'가게 전화번호는 필수입니다'}),
-  address: z.string().min(1, {message:'가게 위치 설정은 필수입니다'}),
+  nickname: z.string().min(1, { message: '닉네임을 입력해주세요' }),
+  store: z.string().min(1, { message: '가게 이름(상호명)을 입력해주세요' }),
+  storeTel: z.string().min(1, { message: '가게 전화번호를 입력해주세요' }),
+  address: z.string().min(1, { message: '가게 위치를 설정해주세요' }),
 });
 
 export type EditProfileInput = z.infer<typeof editProfileSchema>;
