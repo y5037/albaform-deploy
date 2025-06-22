456bfcaab8ea40f4eb708ea3b4736dd088c2ecd0
HyeyeonJung <jhy20306@gmail.com>
2025-06-22 17:51:13 +0900

    fix: auth store - signin fixed

--- src/hooks/mutation/useSignIn.ts
+++ src/hooks/mutation/useSignIn.ts
@@ -1,6 +1,7 @@
 import { useMutation } from '@tanstack/react-query';
 import Cookies from 'js-cookie';
 import { SignInInput } from '@/schemas/signinSchema';
+import { useAuthStore } from '@/stores/useAuthStore';
 import instance from '@/lib/api/api';
 
 const signIn = async ({ email, password }: SignInInput) => {
@@ -23,13 +24,23 @@ export function useSignIn(
   return useMutation<{ user: any }, Error, SignInInput>({
     mutationFn: async (input: SignInInput) => {
       const data = await signIn(input);
-      const user = data.user?.[0];
+      const user = data.user;
 
-      if (!user || user.role.toLowerCase() !== expectedRole) {
+      if (
+        !user ||
+        user.role.toString().toLowerCase() !==
+          expectedRole.toString().toLowerCase()
+      ) {
         throw new Error('이 페이지에서는 해당 역할로 로그인할 수 없습니다.');
       }
       return data;
     },
-    ...options,
+    onSuccess: (data) => {
+      useAuthStore.getState().setUser(data.user);
+      if (options?.onSuccess) options.onSuccess(data);
+    },
+    onError: (error) => {
+      if (options?.onError) options.onError(error);
+    },
   });
 }
